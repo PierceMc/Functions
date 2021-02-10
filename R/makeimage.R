c47d77e4b1e06a386f8ebda53679e1360220366a
Pierce <piercemcnie@pm.me>
2021-02-10 12:19:38 -0500

    Title options on makeimage()

--- R/makeimage.R
+++ R/makeimage.R
@@ -1,13 +1,14 @@
 #' Make raster plot of an OutbreakLandscape file
 #'
-#' @param d Directory containing data
-#' @param V Variable of interest
-#' @param v value of interest
-#' @param s landscape size. Default=200
-#' @param p Species. Default=SBW
-#' @return Histogram of patch sizes
+#' @param d Directory containing data.
+#' @param V Variable of interest.
+#' @param v vector of values. 
+#' @param s landscape size. Default=200.
+#' @param p Species. Default=SBW.
+#' @param t Optional. Title for the plot. Default: False.
+#' @return Raster image of OutbreakLandscape file.
 #' @export
-makeimage <- function(d, V, v, s=200, p='SBW'){
+makeimage <- function(d, V, v, s=200, p='SBW', t=F){
 	Out <- list()
 	for(i in v){
 		tmp <- read.csv(paste0(d, 'OutbreakLandscape', V, i, 'run4.csv', p, '.csv'))
@@ -16,8 +17,16 @@ makeimage <- function(d, V, v, s=200, p='SBW'){
 		colnames(landscapesmall) <- paste0("P", c(1:s))
 		rownames(landscapesmall) <- paste0("p", c(1:s))
 		landscapeplot <- as.data.frame(reshape2::melt(landscapesmall))
-		Out[[which(v == i)]] <- ggplot(landscapeplot, aes(x = Var2, y = Var1)) +   geom_raster(aes(fill=value))+ scale_fill_gradient(low="white", high="black") + labs(x="", y="") + theme_bw()+ theme(legend.position='none', axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.text.y = element_blank(),axis.ticks.y = element_blank()) + ggtitle(paste(V, i))
-
+		if(t == T){ 
+		Out[[which(v == i)]] <- ggplot(landscapeplot, aes(x = Var2, y = Var1)) +   geom_raster(aes(fill=log(value)))+ scale_fill_gradient(low="white", high="green") + labs(x="", y="") + theme_minimal()+ theme(legend.position='none', axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.text.y = element_blank(),axis.ticks.y = element_blank()) + coord_equal() + ggtitle(paste(V, i)) 
+		} else if(is.character(t)){
+		Out[[which(v == i)]] <- ggplot(landscapeplot, aes(x = Var2, y = Var1)) +   geom_raster(aes(fill=log(value)))+ scale_fill_gradient(low="white", high="green") + labs(x="", y="") + theme_minimal()+ theme(legend.position='none', axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.text.y = element_blank(),axis.ticks.y = element_blank()) + ggtitle(t) + coord_equal()
+		} else { 
+		Out[[which(v == i)]] <- ggplot(landscapeplot, aes(x = Var2, y = Var1)) +   geom_raster(aes(fill=log(value)))+ scale_fill_gradient(low="white", high="green") + labs(x="", y="") + theme_minimal()+ theme(legend.position='none', axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.text.y = element_blank(),axis.ticks.y = element_blank()) + coord_equal()
+		}
 	}
 	return(Out)
 }
+
+
+
