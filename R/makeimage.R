d913d7cb0d06ab6e6126e5489a829e852dc388f7
Pierce <piercemcnie@pm.me>
2021-02-10 12:38:54 -0500

    iteration for makeimage

--- R/makeimage.R
+++ R/makeimage.R
@@ -14,7 +14,7 @@ makeimage <- function(d, V, v, s=200, p='SBW', t=F){
 	Out <- list()
 	for(i in v){
 		tmp <- read.csv(paste0(d, 'OutbreakLandscape', V, i, 'run4.csv', p, '.csv'))
-		landscapesquare <- square(tmp[100,])
+		landscapesquare <- square(tmp[50,])
 		landscapesmall <- as.matrix(landscapesquare[1:s,1:s])
 		colnames(landscapesmall) <- paste0("P", c(1:s))
 		rownames(landscapesmall) <- paste0("p", c(1:s))
