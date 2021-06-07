1012259cd539f67199d906fe872ecde394f07fe6
Pierce <piercemcnie@pm.me>
2021-06-08 11:29:40 +1200

    exported mround()

--- R/round.R
+++ R/round.R
@@ -3,6 +3,7 @@
 ####' @param x numeric to round.
 ####' @param base Value to round to.
 ####' @return Rounded value(s) of x
+####' @export
 mround <- function(x,base){
         base*round(x/base)
 }
