2f8bc8fca2f45be1970dcf9e00abd2e7a5ce6337
PierceMc <37709567+PierceMc@users.noreply.github.com>
2023-08-31 09:29:40 -0400

    Update .onLoad.R

--- R/.onLoad.R
+++ R/.onLoad.R
@@ -2,6 +2,7 @@
 #'
 #' @export
 .onLoad <- function(libname = find.package("phd"), pkgname = "phd") {
+	textsize=15
 	theme_set(theme_classic())
 	theme_update(legend.position='none', legend.title=element_text(size=textsize),legend.text=element_text(size=textsize),axis.title=element_text(size=textsize))
 	ggplot <- function(...) ggplot2::ggplot(...) + scale_fill_brewer(palette="Dark2") + scale_color_brewer(palette="Dark2")
